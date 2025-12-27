8acf6cac2b0902ad0d895881d1c11c65ffc8a470
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-28 01:37:51 +0530

    feat: implement application routes with necessary route protectors

--- src/api/v1/application/application.routes.js
+++ src/api/v1/application/application.routes.js
@@ -1,8 +1,40 @@
+// import local modules
+import {
+  acceptApplication,
+  createApplication,
+  rejectApplication,
+  withdrawApplication,
+} from './application.controllers.js';
+import {
+  canUserWithdrawApplication,
+  doesApplicationExistInGroup,
+  isUserAlreadyInAGroup,
+  isUserGroupAdmin,
+  userAlreadyHasAPendingApplication,
+} from '../../../utils/route-protectors/index.js';
+
 // import external modules
 import { Router } from 'express';
 
 // create a new router
 const router = Router({ mergeParams: true });
+
+// @route POST /
+router.post('/', isUserAlreadyInAGroup, userAlreadyHasAPendingApplication, createApplication);
+
+// @route PATCH /accept
+router.patch('/accept', isUserGroupAdmin, acceptApplication);
+
+// @route PATCH /reject
+router.patch('/reject', isUserGroupAdmin, rejectApplication);
+
+// @route PATCH /withdraw
+router.patch(
+  '/withdraw',
+  doesApplicationExistInGroup,
+  canUserWithdrawApplication,
+  withdrawApplication
+);
 
 // export router
 export default router;
