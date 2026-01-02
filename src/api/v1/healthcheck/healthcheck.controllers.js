f118e036026690806464f1c2ac155da5d4bbff14
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-02 17:59:41 +0530

    feat: add detailed documentation for health check controller

--- src/api/v1/healthcheck/healthcheck.controllers.js
+++ src/api/v1/healthcheck/healthcheck.controllers.js
@@ -1,7 +1,22 @@
 // import local modules
 import { APISuccessResponse } from '../../response.api.js';
 
-// @controller GET /
+/**
+ * GET /healthcheck
+ * @security
+ * @summary Health Check Controller
+ * @tags HealthCheck - Health Check Routes
+ * @description Controller to check the health status of the server.
+ * @return {APISuccessResponse} 200 - Successful response
+ * @example response - 200 - Successful response example
+ * {
+ *  "success": true,
+ *  "statusCode": 200,
+ *  "response": {
+ *    "message": "Server is Running"
+ *  }
+ * }
+ */
 export const healthCheck = (_, res) => {
   return res.status(200).json(new APISuccessResponse(200, { message: 'Server is Running' }));
 };
